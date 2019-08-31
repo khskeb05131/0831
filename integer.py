7c615501e49aea85db75d474dd0dc0405730a5ae
khskeb05131 <54461238+khskeb05131@users.noreply.github.com>
2019-08-31 10:57:49 +0900

    Add files via upload

---
+++ integer.py
@@ -0,0 +1,4 @@
+print(1)
+print("{0}".format(x))
+print("{}".format(3**4))
+print("{}".format(int(9.3)/int(2.7)))
---
+++ string.py
@@ -0,0 +1,9 @@
+print("{:s}".format('I\'m enjoying learning Prthon'))
+print ("{0:s}".format("This is a loing string. Without the backslash\
+it would run off of the page on the right in the text editor and be\
+difficult in read and edit. By using the backslash you van split the long\
+string into smaller strings on seperate lines so that the shole string is easy\
+to view in the editor."))
+
+print("{0:s}".format("""You can use triple single quotes
+                     for multi-line comment strings"""))
