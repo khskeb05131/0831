dabfb895726e5997a75f9301910c1acc2f3fde33
khskeb05131 <54461238+khskeb05131@users.noreply.github.com>
2019-08-31 12:29:00 +0900

    Create tuple.py

---
+++ tuple.py
@@ -0,0 +1,11 @@
+#튜플은 값을 변경할 수 없습니다
+#리스트와 동일하지만 값을 변경할 수는 없습니다
+#값을 변경하는 함수를 제공하지 않습니다
+#튜플은 괄호를 이용하여 생성합니다
+my_tuple = ('x','y','z')
+your_tuple = (3,4,5,6,7)
+print(my_tuple)
+print(your_tuple)
+print(my_tuple[1])
+our_tuple = my_tuple + your_tuple
+print(our_tuple)
